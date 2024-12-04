43ec34b232822e7d18da80cb67f59c91747770c5
luch <webwork.s@qq.com>
2024-12-04 16:13:53 +0800

    chore: 增加ts 文件支持

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,28 @@
 
-
-export interface Options {
-    cache: number
-}
+declare module 'single-promises' {
+    export const version: string;
+  
+    export interface SinglePromiseOptions {
+      /** Cache duration in milliseconds, default is 0 (no cache). */
+      cache?: number;
+    }
+  
+    type AsyncFunction<T extends any[], R> = (...args: T) => Promise<R>;
+    type SyncFunction<T extends any[], R> = (...args: T) => R;
+  
+    interface SinglePromiseHandler<T extends any[], R> {
+      /** Clears the cached result. */
+      clear: () => void;
+      /** Updates the cache options. */
+      update: (newOpt?: SinglePromiseOptions) => void;
+      /** The function itself that returns a promise. */
+      (...args: T): Promise<R>;
+    }
+  
+    function singlePromise<F extends SyncFunction<any[], any> | AsyncFunction<any[], any>>(
+      fn: F, 
+      opt?: SinglePromiseOptions
+    ): SinglePromiseHandler<Parameters<F>, ReturnType<F>>;
+  
+    export { singlePromise };
+  }
